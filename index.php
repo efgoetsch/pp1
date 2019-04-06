092920b7ae12278abae002c92b1555e6be36704a
efgoetsch <egoetsch@mail.greenriver.edu>
2019-04-05 23:43:38 -0400

    Second commit

--- index.php
+++ index.php
@@ -7,5 +7,23 @@
 <body>
 <header>Pair Program 1</header>
 
+<?php
+
+    $numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+    
+
+    function printArr($numbers)
+    {
+        foreach($numbers as $number)
+        {
+            echo "$number <br>";
+        }
+    }
+
+    printArr($numbers);
+
+?>
+
 </body>
 </html>
